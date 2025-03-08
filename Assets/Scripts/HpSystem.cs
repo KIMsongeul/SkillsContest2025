0831cc2ece381a24bd997e16c35b8ce8a367e34b
KIMsongeul <0754minjun@gmail.com>
2025-03-08 19:26:50 +0900

    Hp바 UI와 Hpsystem 연동 제작

--- Assets/Scripts/HpSystem.cs
+++ Assets/Scripts/HpSystem.cs
@@ -2,21 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HpSystem : MonoBehaviour
 {
     public int maxHp;
     public int currentHp;
-    public int damage;
+    public int attackDamage;
+    public Slider Hpslider;
 
     private void Start()
     {
         currentHp = maxHp;
     }
 
+    public void UpdateHpBar()
+    {
+        if (Hpslider != null)
+        {
+            Hpslider.value = currentHp;
+        }
+    }
+
     public void Damage(int damage)
     {
         currentHp -= damage;
+        UpdateHpBar();
         if (currentHp <= 0)
         {
             Debug.Log("Died");
