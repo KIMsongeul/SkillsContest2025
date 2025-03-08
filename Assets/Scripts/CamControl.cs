56acff2a77f3190124a33d942b92f975de911dbd
KIMsongeul <0754minjun@gmail.com>
2025-03-08 12:34:45 +0900

    1인칭 시점 제작

--- Assets/Scripts/CamControl.cs
+++ Assets/Scripts/CamControl.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CamControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject target;
+    private Camera cam;
+    private float mouseSpeed = 200f;
+    private float xrot;
+    private float yrot;
+
+    private void Start()
     {
-        
+        cam = Camera.main;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
+        xrot -= mouseY;
+        yrot += mouseX;
+
+        xrot = Mathf.Clamp(xrot, -70f, 70f);
+        cam.transform.rotation = Quaternion.Euler(xrot,yrot,0);
+        target.transform.rotation = Quaternion.Euler(0, yrot, 0);
     }
 }
